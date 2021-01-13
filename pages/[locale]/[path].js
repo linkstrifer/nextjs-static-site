d06cd06663cf6ba705f054f9c710c62683d4e64b
Link Strifer <linkstrifer@outlook.com>
2021-01-13 18:05:46 -0500

    create pages from json with i18n

---
+++ pages/[locale]/[path].js
@@ -0,0 +1,65 @@
+import fs from "fs";
+import Link from "next/link";
+
+const pathsData = JSON.parse(fs.readFileSync("data.json"));
+
+function Page({ content, locale, path, alternateLocales }) {
+  return (
+    <div style={{ margin: "auto", maxWidth: "400px" }}>
+      <h1>{locale.toUpperCase()}</h1>
+      <p>
+        Other locales:
+        {alternateLocales.map((locale) => (
+          <Link key={locale} href={`/${locale}/${path}`}>
+            <a
+              style={{
+                margin: ".5em",
+                border: "1px solid",
+                padding: ".5em",
+              }}
+            >
+              {locale}
+            </a>
+          </Link>
+        ))}
+      </p>
+      <p>{content}</p>
+    </div>
+  );
+}
+
+export const getStaticPaths = async () => {
+  const paths = [];
+
+  for (const [path, data] of Object.entries(pathsData)) {
+    data.forEach(({ locale }) => {
+      const localeData = {
+        params: {
+          path,
+          locale,
+        },
+      };
+
+      paths.push(localeData);
+    });
+  }
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params: { locale, path } }) => {
+  const pathData = pathsData[path];
+  const { content } = pathData.find((pathData) => pathData.locale === locale);
+  const alternateLocales = pathData
+    .map(({ locale }) => locale)
+    .filter((alternateLocale) => alternateLocale !== locale);
+
+  return {
+    props: { locale, path, content, alternateLocales },
+  };
+};
+
+export default Page;
